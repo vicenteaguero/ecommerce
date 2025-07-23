96d99dc587108529b573107d40003614d718ad48
Vicente Agüero <vicenteaguero@uc.cl>
2025-07-22 23:47:19 -0400

    feat(streamlit): :art: Add better UI

--- app/src/layout.py
+++ app/src/layout.py
@@ -5,7 +5,6 @@ import streamlit as st
 from src.params import PATHS, GITHUB_URL, REPORT_URL_MAIL, ABOUT_TEXT
 
 def setup_layout(page_title: str):
-    """Set up the layout of the Streamlit app."""
     st.set_page_config(
         layout='wide',
         page_title=page_title,
@@ -17,46 +16,40 @@ def setup_layout(page_title: str):
             'About': ABOUT_TEXT,
         },
     )
-
-    st.logo(
-        image=PATHS['logo'],
-        link=GITHUB_URL,
-    )
+    st.logo(image=PATHS['logo'], link=GITHUB_URL)
     st.markdown("""
         <style>
-            img.stLogo {display: block; height: 50px; margin: 0}
+            @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
+
+            @font-face {
+                font-family: 'Averta';
+                src: url('https://raw.githubusercontent.com/google/fonts/main/ofl/sourcesanspro/SourceSansPro-Regular.ttf') format('truetype');
+                font-weight: 400;
+                font-style: normal;
+                font-display: swap;
+            }
+
+            html, body, [class*="css"] {
+                font-family: 'Averta', sans-serif !important;
+            }
+
+            img.stLogo {
+                display: block;
+                width: 100%;
+                height: auto;
+                margin-top: 20px;
+            }
         </style>
     """, unsafe_allow_html=True)
 
 def setup_pages():
-    """
-    Set up the pages of the Streamlit app.
-
-    Returns
-    -------
-    None
-    """
     pages = {
         'General': [
-            st.Page(
-                page=PATHS['pages']['home'],
-                title='Home',
-                icon='🏠',
-            ),
-            st.Page(
-                page=PATHS['pages']['docs'],
-                title='Documentation',
-                icon='📚',
-            ),
+            st.Page(page=PATHS['pages']['home'], title='Home', icon='🏠'),
+            st.Page(page=PATHS['pages']['docs'], title='Documentation', icon='📚'),
         ],
         'Features': [
-            st.Page(
-                page=PATHS['pages']['cart'],
-                title='Shopping Cart',
-                icon='🛒',
-            ),
+            st.Page(page=PATHS['pages']['cart'], title='Shopping Cart', icon='🛒'),
         ],
     }
-
-    pg = st.navigation(pages)
-    pg.run()
+    st.navigation(pages).run()
